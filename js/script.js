784868463a84bc747a8792dd2f5c0e6dbfcab1c4
Denys Zhorov <2013dvl@gmail.com>
2019-04-21 12:09:09 +0200

    add script POST Form server

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 window.addEventListener("DOMContentLoaded", function () {
-    "use strict";
+    'use strict';
     ///////////////////////tab menu
     let tab = document.querySelectorAll(".info-header-tab"),
         info = document.querySelector(".info-header"),
@@ -13,6 +13,7 @@ window.addEventListener("DOMContentLoaded", function () {
     }
 
     hideTabContent(1);
+
     function showTabContent(a) {
         if (tabContent[a].classList.contains("hide")) {
             tabContent[a].classList.remove("hide");
@@ -27,7 +28,7 @@ window.addEventListener("DOMContentLoaded", function () {
                 if (target == tab[i]) {
                     hideTabContent(0);
                     showTabContent(i);
-                    
+
                 }
             }
         }
@@ -74,38 +75,91 @@ window.addEventListener("DOMContentLoaded", function () {
             if (t.total <= 0) {
                 clearInterval(timeInterval);
             }
-
         }
-
     }
     setClock('timer', deadline);
 
     ///////////modal
     let btnMore = document.querySelector(".more"),
-    modal = document.querySelector(".overlay"),
-    btnClose = document.querySelector(".popup-close"),
-    descBtn =document.querySelectorAll(".description-btn");
+        modal = document.querySelector(".overlay"),
+        btnClose = document.querySelector(".popup-close"),
+        descBtn = document.querySelectorAll(".description-btn");
 
- 
-
-
-   function openModal(){
-    modal.style.display = "block";
-    btnMore.classList.add("more-splash");
-    document.body.style.overflow = "hidden";
+    function openModal() {
+        modal.style.display = "block";
+        btnMore.classList.add("more-splash");
+        document.body.style.overflow = "hidden";
     }
-    btnMore.addEventListener('click', openModal); 
-     descBtn[0].addEventListener('click', openModal); 
-     descBtn[1].addEventListener('click', openModal); 
-     descBtn[2].addEventListener('click', openModal); 
-     descBtn[3].addEventListener('click', openModal); 
-    console.log(descBtn);
- 
 
-   btnClose.addEventListener('click', function(){
-    modal.style.display = "none";
-    btnMore.classList.remove("more-splash");
-    document.body.style.overflow = " ";
-   });
-  
+    btnMore.addEventListener('click', openModal);
+    descBtn[0].addEventListener('click', openModal);
+    descBtn[1].addEventListener('click', openModal);
+    descBtn[2].addEventListener('click', openModal);
+    descBtn[3].addEventListener('click', openModal);
+
+    btnClose.addEventListener('click', function () {
+        modal.style.display = "none";
+        btnMore.classList.remove("more-splash");
+        document.body.style.overflow = "";
+        form.removeChild(statusMessage);
+    });
+
+    ////////////////Form
+    let message = {
+        loading: "Загрузка...",
+        success: "Спасибо! Скоро мы с вами свяжемся!",
+        failure: "Что-то пошло не так..."
+    };
+    let form = document.querySelector('.main-form'),
+        contactForm = document.querySelector('.form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status');
+
+    function postServerForm(event, targetEvent) {
+        event.preventDefault();
+        console.log(event.target);
+        targetEvent.appendChild(statusMessage);
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+        let formData = new FormData(targetEvent);
+        console.log(formData);
+        let obj = {};
+        formData.forEach(function (value, key) {
+            obj[key] = value;
+        });
+        console.log(obj);
+        let json = JSON.stringify(obj);
+        console.log(json);
+        request.send(json);
+        request.addEventListener('readystatechange', function () {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+        console.log(7);
+    }
+
+    form.addEventListener('submit', function (event) {
+        let eventForm = event,
+            target = event.target;
+        postServerForm(eventForm, target);
+    });
+
+    contactForm.addEventListener('submit', function (event) {
+        let eventForm = event,
+            target = event.target;
+        postServerForm(eventForm, target);
+    });
+
 });
